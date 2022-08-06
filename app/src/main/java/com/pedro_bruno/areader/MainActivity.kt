15b3849c329f1c8e41534eb7f6f3a6e4e21b993f
Pedro Bruno <pbruno1283@gmail.com>
2022-08-06 11:49:57 -0300

    Setting Up Di
    Criada a arquitetura de pastas e o arquivo AppModule para Injeção de dependências.

--- app/src/main/java/com/pedro_bruno/areader/MainActivity.kt
+++ app/src/main/java/com/pedro_bruno/areader/MainActivity.kt
@@ -13,31 +13,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.firebase.firestore.FirebaseFirestore
 import com.pedro_bruno.areader.ui.theme.AReaderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AReaderTheme {
-
-
-                val db = FirebaseFirestore.getInstance()
-                val user: MutableMap<String, Any> = HashMap()
-                user["firstName"] = "Pedro"
-                user["lastName"] = "Bruno"
-
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    db.collection("users")
-                        .add(user)
-                        .addOnSuccessListener {
-                            Log.d("FB", "onCreate: ${it.id}")
-                        }.addOnFailureListener {
-                            Log.d("FB", "onCreate:${it} ")
-                        }
                     Greeting("Android")
                 }
             }
